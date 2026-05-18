ffaaa8d481b4098231e17a41c32531688c117be0
Yulia Fig <fihlevska@gmail.com>
2026-05-18 20:50:39 +0200

    hw4-react

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,31 +8,35 @@ import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
 import type { Movie } from "../../types/movie";
-import {movieService} from '../../services/movieService'
+import { movieService } from "../../services/movieService";
 import { Toaster } from "react-hot-toast";
 import { notifyNoMovies } from "../../services/toast";
 import Paginate from "../ReactPaginate/ReactPaginate";
 export default function App() {
-  
   const [isModal, setIsModal] = useState<Movie | null>(null);
-  
+
   const [isVideos, setIsVideos] = useState<Movie[]>([]);
   const [isSearch, setIsSearch] = useState("");
   const [isPage, setIsPage] = useState(1);
 
   const { data, error, isLoading, isError, isSuccess } = useQuery({
-  queryKey: ['myVideoKey', isSearch, isPage], 
-  queryFn: () => movieService(isSearch, isPage), 
-  enabled: isSearch !== "", // якщо пошукове поле для вводу пусте, запит не робиться 
-  placeholderData: keepPreviousData,
-});
+    queryKey: ["myVideoKey", isSearch, isPage],
+    queryFn: () => movieService(isSearch, isPage),
+    enabled: isSearch !== "", // якщо пошукове поле для вводу пусте, запит не робиться
+    placeholderData: keepPreviousData,
+  });
 
   const closeModal = () => {
     setIsModal(null);
   };
-  
-const results = data?.results ?? [];
+
+  const results = data?.results ?? [];
   const totalPages = data?.total_pages ?? 0;
+  useEffect(() => {
+    if (data?.results && data.results.length === 0) {
+      notifyNoMovies();
+    }
+  }, [data]);
   return (
     <div className={css.app}>
       <SearchBar
@@ -43,7 +47,11 @@ const results = data?.results ?? [];
         }}
       />
       {isSuccess && totalPages > 1 && (
-        <Paginate totalPages={totalPages} currentPage={isPage} onPageChange={setIsPage} />
+        <Paginate
+          totalPages={totalPages}
+          currentPage={isPage}
+          onPageChange={setIsPage}
+        />
       )}
       {!isLoading && !isError && results.length > 0 && (
         <MovieGrid
@@ -55,9 +63,8 @@ const results = data?.results ?? [];
       )}
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
-      {isModal && <MovieModal movie={isModal}
-          onClose={closeModal} />}
-          <Toaster position="top-center" reverseOrder={false} />
+      {isModal && <MovieModal movie={isModal} onClose={closeModal} />}
+      <Toaster position="top-center" reverseOrder={false} />
     </div>
   );
 }
